fa5b69fbe206be842afc34150aedd1606959ffef
Mohame Obaya <mohamed.ali.obaya@gmail.com>
2020-09-25 02:54:23 +0200

    JS_Task2

---
+++ IDE_javaScript_TASK2/JS_TASK2_pt2/index.js
@@ -0,0 +1,16 @@
+var item1 = document.getElementById('1'),
+    item2 = document.getElementById('2'),
+    item3 = document.getElementById('3'),
+    item4 = document.getElementById('4');
+item1.addEventListener('click', function (e) {
+    document.getElementById('result').innerText = 'Item_1 : Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quisquam dolorum laboriosam laudantium repellendus assumenda odit quo a cupiditate exercitationem. Laborum eum qui quasi autem est facere hic. Iure, velit!';
+});
+item2.addEventListener('click', function (e) {
+    document.getElementById('result').innerText = 'Item_2 : Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quisquam dolorum laboriosam laudantium repellendus assumenda odit quo a cupiditate exercitationem. Laborum eum qui quasi autem est facere hic. Iure, velit!';
+});
+item3.addEventListener('click', function (e) {
+    document.getElementById('result').innerText = 'Item_3 : Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quisquam dolorum laboriosam laudantium repellendus assumenda odit quo a cupiditate exercitationem. Laborum eum qui quasi autem est facere hic. Iure, velit!';
+});
+item4.addEventListener('click', function (e) {
+    document.getElementById('result').innerText = 'Item_4 : Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quisquam dolorum laboriosam laudantium repellendus assumenda odit quo a cupiditate exercitationem. Laborum eum qui quasi autem est facere hic. Iure, velit!';
+});
